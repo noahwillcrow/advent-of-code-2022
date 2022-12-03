8d9b69d76ff87f16946234582f0fc706c779b7dc
Noah Crowley <noahwillcrow@gmail.com>
2022-12-03 18:10:36 -0500

    slight improvement to part two

--- src/day-01/DayOnePartTwo.purs
+++ src/day-01/DayOnePartTwo.purs
@@ -5,9 +5,10 @@ module DayOnePartTwo
 
 import Prelude
 
-import Data.Array (insert, take, reverse)
+import Data.Array (insertBy, take)
 import Data.Foldable (sum)
 import Data.Int (decimal, toStringAs)
+import Data.Ordering (invert)
 import Data.String.Utils (lines)
 import Data.Tuple (Tuple(..))
 import DayOneCommon (countCaloriesForNextElf)
@@ -16,14 +17,17 @@ import Effect.Console (log)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+invertCompare :: forall a. Ord a => a -> a -> Ordering
+invertCompare a b = invert $ compare a b
+
 handleInputLines :: Array String -> Array Int-> Array Int
 handleInputLines inputArray outputArray = case (countCaloriesForNextElf inputArray 0) of
   Tuple _ [] -> outputArray
-  Tuple newValue remainingInputArray -> handleInputLines remainingInputArray (insert newValue outputArray)
+  Tuple newValue remainingInputArray -> handleInputLines remainingInputArray (insertBy invertCompare newValue outputArray)
 
 runDayOnePartTwo :: Effect Unit
 runDayOnePartTwo = do
   inputFileContents <- readTextFile UTF8 "./inputs/day-one-input.txt"
-  log (toStringAs decimal (sum (take 3 (reverse (handleInputLines (lines inputFileContents) [])))))
+  log (toStringAs decimal (sum (take 3 (handleInputLines (lines inputFileContents) []))))
 
 
