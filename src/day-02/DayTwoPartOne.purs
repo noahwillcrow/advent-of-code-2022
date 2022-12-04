84fc225327b279b6c88eaf93b4cdccec8e735c46
Noah Crowley <noahwillcrow@gmail.com>
2022-12-04 01:29:33 -0500

    no common

--- src/day-02/DayTwoPartOne.purs
+++ src/day-02/DayTwoPartOne.purs
@@ -9,12 +9,22 @@ import Data.Array (head, tail)
 import Data.Int (decimal, toStringAs)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String.Utils (lines)
-import DayTwoCommon (determineScoreForRound)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+determineScoreForRound :: String -> Int
+determineScoreForRound "A X" = 3 + 1
+determineScoreForRound "A Y" = 6 + 2
+determineScoreForRound "A Z" = 0 + 3
+determineScoreForRound "B X" = 0 + 1
+determineScoreForRound "B Y" = 3 + 2
+determineScoreForRound "B Z" = 6 + 3
+determineScoreForRound "C X" = 6 + 1
+determineScoreForRound "C Y" = 0 + 2
+determineScoreForRound "C Z" = 3 + 3
+determineScoreForRound _ = 0
 
 handleInputLines :: Array String -> Int -> Int
 handleInputLines inputArray currentScore = case (head inputArray) of
